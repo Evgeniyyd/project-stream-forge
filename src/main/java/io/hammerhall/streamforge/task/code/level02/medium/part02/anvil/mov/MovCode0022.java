f1f6e0a9ffc7517f1cc17dfe6f4a57cffa8b36b2
Evgeniyyd./hammerhallMovCode0022 <brai.kel@icloud.com>
2026-08-11 00:37:00 +0400

    готово

--- src/main/java/io/hammerhall/streamforge/task/code/level02/medium/part02/anvil/mov/MovCode0022.java
+++ src/main/java/io/hammerhall/streamforge/task/code/level02/medium/part02/anvil/mov/MovCode0022.java
@@ -6,11 +6,15 @@ import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import io.hammerhall.streamforge.domain.movie.Director;
 import io.hammerhall.streamforge.domain.movie.Movie;
 import io.hammerhall.streamforge.task.Base;
+
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.TreeMap;
+import java.util.stream.Collectors;
+
 import lombok.NonNull;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -30,23 +34,27 @@ public class MovCode0022 extends Base {
      * значение — количество его фильмов
      */
     public TreeMap<String, Long> task(@NonNull Collection<Movie> movies) {
-        throw new UnsupportedOperationException("Реализуйте метод");
+        return movies.stream().flatMap(genre -> genre.getDirectors().stream())
+                .map(Director::getFullName)
+                .collect(Collectors.groupingBy(name -> name, TreeMap::new, Collectors.counting()));
+
     }
 
-    @Test
-    void runTask() {
-        Collection<Movie> movies = movies();
 
-        TreeMap<String, Long> movieCountByDirectorName = task(movies);
+@Test
+void runTask() {
+    Collection<Movie> movies = movies();
 
-        assertNotNull(movieCountByDirectorName);
-        assertFalse(movieCountByDirectorName.isEmpty());
-        assertInstanceOf(TreeMap.class, movieCountByDirectorName);
+    TreeMap<String, Long> movieCountByDirectorName = task(movies);
 
-        assertTrue(movieCountByDirectorName.values().stream().allMatch(c -> c != null && c > 0));
-        assertEquals(movieCountByDirectorName.keySet().stream().sorted().toList(), new ArrayList<>(movieCountByDirectorName.keySet()));
+    assertNotNull(movieCountByDirectorName);
+    assertFalse(movieCountByDirectorName.isEmpty());
+    assertInstanceOf(TreeMap.class, movieCountByDirectorName);
 
-        movieCountByDirectorName.forEach((directorName, movieCount) ->
-                System.out.printf("%-25s %d%n", directorName, movieCount));
-    }
+    assertTrue(movieCountByDirectorName.values().stream().allMatch(c -> c != null && c > 0));
+    assertEquals(movieCountByDirectorName.keySet().stream().sorted().toList(), new ArrayList<>(movieCountByDirectorName.keySet()));
+
+    movieCountByDirectorName.forEach((directorName, movieCount) ->
+            System.out.printf("%-25s %d%n", directorName, movieCount));
+}
 }
